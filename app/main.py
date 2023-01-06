a58a9a7b98045a3cbc6ff7d3afc810b3bf5a6027
AMihevc <mihevc.anze@gmail.com>
2023-01-06 13:58:53 +0100

    improved health and minor changes

--- app/main.py
+++ app/main.py
@@ -1,11 +1,12 @@
 from typing import List
 from fastapi import APIRouter, HTTPException, Path, Depends, FastAPI
 from sqlalchemy.orm import Session
-from .databaseDN import SessionLocal, engine
+from .databaseDN import SessionLocal, engine, username, password, hostname, database
 
 #za loging
 import sentry_sdk
 from sentry_sdk import set_level
+
 
 #za metrike
 from prometheus_fastapi_instrumentator import Instrumentator
@@ -16,6 +17,10 @@ from app import crudDN, modelsDN, schemasDN
 #za health and livnes info 
 from fastapi_health import health
 from datetime import datetime
+import psycopg2
+
+#za middleware
+from fastapi.middleware.cors import CORSMiddleware
 
 #------LOGGING------
 # enable logging on Sentry
@@ -33,9 +38,27 @@ def get_date_and_time():
     dt_string = now.strftime("%d/%m/%Y %H:%M:%S")
     return dt_string
 
-
+#------APP-INIT------
 #inicializiraj app
-app = FastAPI()
+app = FastAPI(
+    title="Kosarice",
+    description="Mikrostoritev, ki skrbi za kosarice izdelkov in \
+        primerjavo cen teh izdelkov med različnimi trgovinami",
+    root_path="/kosaricems",
+    docs_url="/openapi",
+)
+
+#------MIDDLEWARE------
+origins = [
+    ""
+]
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=[""],
+    allow_headers=["*"],
+)
 
 #------METRIKE------ 
 # za metrike izpostavi /metrics
@@ -48,26 +71,53 @@ modelsDN.Base.metadata.create_all(bind=engine)
 
 #------HEALTH and LIVNES------
 
+#naredi dict za prikazat health in livenes checks
+async def health_success_failure_handler(**conditions):
+    rez = {"status": "UP", "checks": []}
+    for cond in conditions:
+        to_add = {
+            "name": cond,
+            "status": conditions[cond]
+        }
+        rez["checks"].append(to_add)
+        if not conditions[cond]:
+            rez["status"] = "DOWN"
+    return rez
+
+#pridobi stanje povezljivosti z bazo 
+def check_db_connection():
+    try:
+        #se poskusi povezat na bazo
+        conn = psycopg2.connect(f"dbname={database} user={username} host={hostname} password={password} "
+                                f"connect_timeout=1")
+        
+        #zapre povezavo z bazo
+        conn.close()
+        
+        #če si se povezal na bazo potem vrni True
+        return True
+    except:
+        print("I am unable to connect to the database")
+        return False
+
+
 #pridobi stanje mikrostoritve
 def get_ms_status():
-    if broken:
-        return {"status": "The microservice is BROKEN", 
-                "date": get_date_and_time()}
-    return {"status": "The microservice is working",
-            "date": get_date_and_time()}
-
+    global broken
+    return not broken
 
 #Preveri ali je mikrostoritev živa
 def is_ms_alive():
-    if broken:
+    if broken or not database_working:
         return False
     return True
 
-#dodaj pot do preverjanja health in livness
-app.add_api_route("/health/liveness", health([is_ms_alive, get_ms_status]))
 
 #globalna spremenljivka za "pokvarit" storitev (for demo)
 broken = False
+
+#globalna spremenljivka za delovanje baze
+database_working = True
 
 #"pokvari" mikrostoritev
 @app.post("/break")
@@ -82,6 +132,17 @@ async def unbreak_app():
     global broken 
     broken = False
     return {"The ms is fixed"}
+
+
+#not sure kako točno tole dela odstranil en check(za prostor v bazi)
+health_handler = health([get_ms_status, check_db_connection],
+                        success_handler=health_success_failure_handler,
+                        failure_handler=health_success_failure_handler)
+
+#doda poti za health in liveness 
+app.add_api_route("/health/liveness", health_handler, name="check liveness")
+app.add_api_route("/health/readiness", health_handler, name="check readiness")
+
 
 #------ CORE funkcionalnosti ------
 
@@ -117,7 +178,7 @@ async def get_kosarica(id: int):
 
 
 #košarici s podanim id-jem dodaj nek izdelek
-#TODO posodobi, da bo izdelek dodan iz podatkov iz Timotove MS (to na frontendu al tuki?)
+#TODO na fronendu se bo dodajal ta objekt i guess
 @app.post("/kosarice/{kosarica_id}/izdelki/", response_model=schemasDN.Izdelek)
 def create_izdelek_for_kosarica(kosarica_id: int, izdelek: schemasDN.IzdelekCreate, db: Session = Depends(get_db)):
     return crudDN.create_izdelek_kosarico(db=db, izdelek=izdelek, kosarica_id=kosarica_id)
